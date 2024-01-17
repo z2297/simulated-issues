de339c38fbca7a294867cf2d71f42aa03d19955a
John F.P. Dazey <john.dazey@leadingagile.com>
2024-01-17 14:20:11 -0500

    model tests

---
+++ app/src/app/models/navigation-section.model.spec-jest.ts
@@ -0,0 +1,19 @@
+import { NavigationProperty } from "./navigaion-property.model";
+import { NavigationSection } from "./navigation-section.model";
+
+describe('NavigationSection', () => {
+    it('should create an instance', () => {
+        expect(new NavigationSection()).toBeTruthy();
+    });
+
+    it('should accept values in the constructor', () => {
+        let navigationSection = new NavigationSection({
+            id: 1,
+            name: 'test',
+            navigationProperties: [ new NavigationProperty({ id: 1, name: 'test', route: 'test' }) ]
+        });
+        expect(navigationSection.id).toEqual(1);
+        expect(navigationSection.name).toEqual('test');
+        expect(navigationSection.navigationProperties.length).toEqual(1);
+    });
+});
