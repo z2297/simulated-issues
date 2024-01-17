3a3e3cbe0c48448a7ada58353ad7f7faf5ce8557
SSD\mcmurryz <Zach.McMurry@flightsafety.com>
2024-01-17 12:01:04 -0500

    refactor: sidebar

--- app/src/app/components/sidebar/sidebar.component.ts
+++ app/src/app/components/sidebar/sidebar.component.ts
@@ -12,6 +12,27 @@ import { NavigationSection } from '../../models/navigation-section.model';
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
-  constructor() {}
+  expandedSections: any[] = [];  
 
+  expandNavigationSection(section: any): void {
+    if(this.sectionExpanded(section)) {
+      this.removeSectionFromExpandedList(section);
+      return;
+    }
+
+    this.expandedSections.push(section);
+  }
+
+  isExpanded(section: any): boolean {
+    return this.sectionExpanded(section);
+  }
+
+  private sectionExpanded(section: any): boolean {
+    return this.expandedSections.findIndex(s => s === section) !== -1;
+  }
+
+  private removeSectionFromExpandedList(section: any): void {
+    const index = this.expandedSections.findIndex(s => s === section);
+    this.expandedSections.splice(index, 1);
+  }
 }
